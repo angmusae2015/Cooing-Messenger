c58414022a515e90910ed7f93937e23f9189a417
이한 <ihan@ihan-ui-MacBookAir.local>
2022-03-17 16:18:56 +0900

    removed unused files

--- source/Cooing Manager/messenger.py
+++ source/Cooing Manager/messenger.py
@@ -6,13 +6,13 @@ wb = load_workbook('책배송-일지.xlsx', data_only=True)
 
 class dropdown:
     def __init__(self):
-        self.menus = []
-        self.selectedMenu = None
+        self.menus = []     # list of menus(dropdown.menu)
+        self.selectedMenu = None    # selected menu(dropdown.menu)
 
-    def add_menus(self, title, value):
+    def add_menus(self, title, value):  # add menus
         self.menus.append(self.menu(title, value))
 
-    def select(self):
+    def select(self):   # ask which menu user will select
         print("Type index")
         for i in range(0, len(self.menus)):
             print(f"{i+1}. {self.menus[i].title}")
@@ -21,25 +21,25 @@ class dropdown:
         self.selectedMenu = self.menus[index-1]
         print(f"You've selected {self.selectedMenu.title}")
 
-    class menu:
+    class menu:     # menu has two attributes: title(str), value
         def __init__(self, title, value):
-            self.title = title
-            self.value = value
+            self.title = title  # title is a string that will be shown to user
+            self.value = value  # value is a real data of menu
 
 
 class tab:
     def __init__(self):
         self.selected_sheet = None
-        self.selected_date = None   # selected date(merged cell range)
+        self.selected_date = None   # selected date(MergedCellRange)
 
-    def select_sheet(self):
+    def select_sheet(self):     # select sheet
         dd = dropdown()
         for sheet in wb.worksheets:
             dd.add_menus(sheet.title, sheet)
         dd.select()
         self.selected_sheet = dd.selectedMenu.value
 
-    def get_departure_date(self):
+    def get_departure_date(self):   # return every merged date cell's ranges(list of MergedCellRanges)
         departureDateCells = []
         for mergedCell in self.selected_sheet.merged_cells.ranges:
             if mergedCell.start_cell.column == 1:
@@ -47,7 +47,7 @@ class tab:
 
         return departureDateCells
 
-    def select_date(self):
+    def select_date(self):  # select merged date cell's range(MergedCellRange)
         departureDateCells = self.get_departure_date()
         dd = dropdown()
         for dateCell in departureDateCells:
