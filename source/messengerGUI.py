2d5c5814f28c6d98b5fc8edbf0ec17d313c9b181
angmusae2015 <angmusae2015@gmail.com>
2022-03-23 01:34:02 +0900

    added GUI interface in messengerGUI.py

--- source/messengerGUI.py
+++ source/messengerGUI.py
@@ -1,19 +1,94 @@
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import *
 import sys
 
 
-class MyApp(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
+def msgConfigLayout(parent):
+    layout = QGridLayout()
 
-    def initUI(self):
-        self.setWindowTitle('Cooing Messenger')
-        self.move(300, 300)
-        self.resize(400, 200)
-        self.show()
+    layout.addWidget(QLabel('발신번호 선택'), 0, 0)
+
+    selectNumBox = QComboBox()
+    selectNumBox.setPlaceholderText("발신번호 선택")
+    layout.addWidget(selectNumBox, 1, 0)
+    layout.addWidget(QLabel('예약 시간 선택'), 0, 1)
+
+    selectReserveTimeBox = QDateTimeEdit()
+    layout.addWidget(selectReserveTimeBox, 1, 1)
+
+    return layout
 
 
-app = QApplication(sys.argv)
-ex = MyApp()
-sys.exit(app.exec_())
+def fileSelectLayout(parent):
+    layout = QGridLayout()
+    label = QLabel("일지 파일 선택(.xlsx)")
+    layout.addWidget(label, 0, 0, 0, 2)
+
+    findButton = QPushButton("찾기", parent)
+    layout.addWidget(findButton, 1, 0)
+
+    fileNameViewer = QTextBrowser()
+    fileNameViewer.setFixedHeight(findButton.height())
+    layout.addWidget(fileNameViewer, 1, 1)
+
+    return layout
+
+
+def sheetSelectLayout(parent):
+    layout = QVBoxLayout()
+    layout.addWidget(QLabel("불러올 시트 선택"))
+    layout.addWidget(QComboBox())
+
+    return layout
+
+
+def selectionLayout(parent):
+    layout = QVBoxLayout()
+    layout.addLayout(msgConfigLayout(parent))
+    layout.addLayout(fileSelectLayout(parent))
+    layout.addLayout(sheetSelectLayout(parent))
+
+    return layout
+
+
+def msgPrevLayout(parent):
+    layout = QHBoxLayout()
+    layout.addLayout(selectionLayout(parent))
+    layout.addWidget(QTextBrowser())
+
+    return layout
+
+
+def scheduleTableLayout(parent):
+    layout = QVBoxLayout()
+    layout.addLayout(msgPrevLayout(parent))
+    layout.addWidget(QTableWidget(10, 10))
+
+    return layout
+
+
+app = QApplication([])
+
+
+"""
+selectNumLabel = QLabel()
+selectNumLabel.setText("발신번호 선택")
+
+selectReserveTimeLabel = QLabel()
+selectReserveTimeLabel.setText("예약 시간 선택")
+
+selectNum = QComboBox()
+selectNum.setPlaceholderText("발신번호 선택")
+
+selectReserveTime = QDateTimeEdit()
+
+msgConfig = QGridLayout()
+msgConfig.addWidget(selectNumLabel, 0, 0)
+msgConfig.addWidget(selectNum, 1, 0)
+msgConfig.addWidget(selectReserveTimeLabel, 0, 1)
+msgConfig.addWidget(selectReserveTime, 1, 1)
+"""
+
+window = QWidget()
+window.setLayout(scheduleTableLayout(window))
+window.show()
+app.exec_()
