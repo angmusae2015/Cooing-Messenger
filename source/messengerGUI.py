c45880e7f4c33727f0a754f303fdfad3e9309477
angmusae2015 <angmusae2015@gmail.com>
2022-03-24 02:41:42 +0900

    Completed shiping alarm program's GUI

--- source/messengerGUI.py
+++ source/messengerGUI.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import QMargins
 import sys
 
 
@@ -8,65 +9,108 @@ def msgConfigLayout(parent):
     layout.addWidget(QLabel('발신 번호 선택'), 0, 0)
 
     selectNumBox = QComboBox()
-    selectNumBox.setPlaceholderText("발신 번호 선택")
     layout.addWidget(selectNumBox, 1, 0)
     layout.addWidget(QLabel('예약 시간 선택'), 0, 1)
+    layout.addWidget(QCheckBox(), 0, 2)
 
     selectReserveTimeBox = QDateTimeEdit()
-    layout.addWidget(selectReserveTimeBox, 1, 1)
+    layout.addWidget(selectReserveTimeBox, 1, 1, 1, 2)
+
+    return layout
+
+
+def fileSelectButtonLayout(parent):
+    layout = QHBoxLayout()
+
+    findButton = QPushButton("찾기", parent)
+    findButton.setFixedWidth(50)
+    layout.addWidget(findButton)
+
+    fileNameViewer = QTextBrowser()
+    fileNameViewer.setFixedHeight(findButton.height() - 5)
+    layout.addWidget(fileNameViewer)
 
     return layout
 
 
 def fileSelectLayout(parent):
-    layout = QGridLayout()
+    layout = QVBoxLayout()
+
     label = QLabel("일지 파일 선택(.xlsx)")
-    layout.addWidget(label, 0, 0, 0, 1)
-
-    space = QSpacerItem(100, 30)
-    layout.addItem(space, 1, 0, 1, 1)
-
-    findButton = QPushButton("찾기", parent)
-    layout.addWidget(findButton, 2, 0)
-
-    fileNameViewer = QTextBrowser()
-    fileNameViewer.setFixedHeight(findButton.height())
-    layout.addWidget(fileNameViewer, 2, 1)
+    layout.addWidget(label)
+    layout.addLayout(fileSelectButtonLayout(parent))
 
     return layout
 
 
 def sheetSelectLayout(parent):
     layout = QVBoxLayout()
+
     layout.addWidget(QLabel("불러올 시트 선택"))
     layout.addWidget(QComboBox())
 
     return layout
 
 
-def selectionLayout(parent):
+def sendButtonLayout(parent):
+    layout = QHBoxLayout()
+
+    layout.addStretch(1)
+
+    sendButton = QPushButton('문자 보내기', parent)
+    sendButton.setFixedWidth(150)
+    sendButton.setFixedHeight(80)
+    layout.addWidget(sendButton)
+
+    return layout
+
+
+def selectionAndSendLayout(parent):
     layout = QVBoxLayout()
+
     layout.addLayout(msgConfigLayout(parent))
+    layout.addStretch(1)
+    # layout.addSpacerItem(QSpacerItem(1, 5))
     layout.addLayout(fileSelectLayout(parent))
+    layout.addStretch(1)
+    # layout.addSpacerItem(QSpacerItem(1, 5))
     layout.addLayout(sheetSelectLayout(parent))
+    layout.addStretch(1)
+    # layout.addSpacerItem(QSpacerItem(1, 5))
+    layout.addLayout(sendButtonLayout(parent))
+    layout.addStretch(0)
 
     return layout
 
 
 def msgPrevLayout(parent):
-    layout = QHBoxLayout()
-    layout.addLayout(selectionLayout(parent))
+    layout = QVBoxLayout()
+
+    layout.addWidget(QLabel('문자 미리보기'))
+
+    msgSelection = QComboBox()
+    layout.addWidget(msgSelection)
 
     msgPreview = QTextBrowser()
+    msgPreview.setFixedHeight(200)
     layout.addWidget(msgPreview)
+
+    return layout
+
+
+def msgLayout(parent):
+    layout = QHBoxLayout()
+    layout.addLayout(selectionAndSendLayout(parent))
+    layout.addLayout(msgPrevLayout(parent))
 
     return layout
 
 
 def scheduleTableLayout(parent):
     layout = QVBoxLayout()
-    layout.addLayout(msgPrevLayout(parent))
-    layout.addWidget(QTableWidget(10, 10))
+
+    layout.addLayout(msgLayout(parent))
+    layout.addWidget(QTableWidget(10, 5), 10)
 
     return layout
 
@@ -74,5 +118,8 @@ def scheduleTableLayout(parent):
 app = QApplication([])
 window = QWidget()
 window.setLayout(scheduleTableLayout(window))
+window.setGeometry(500, 500, 600, 600)
+window.setFixedWidth(600)
+
 window.show()
 app.exec_()
