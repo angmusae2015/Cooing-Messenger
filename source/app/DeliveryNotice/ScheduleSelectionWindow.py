aa06302d083ad53b8435b085a9e27f16625e1c21
이한 <ihan@ihan-ui-MacBookAir.local>
2022-03-30 19:49:04 +0900

    added function that loads child's name and adds to table

--- source/app/DeliveryNotice/ScheduleSelectionWindow.py
+++ source/app/DeliveryNotice/ScheduleSelectionWindow.py
@@ -8,6 +8,7 @@ data_path = "../data/"
 schedule_path = data_path + "Schedule.csv"
 book_list_path = data_path + "BookList.csv"
 series_list_path = data_path + "SeriesList.csv"
+member_path = data_path + "Member.csv"
 
 
 class Layout(QGridLayout):
@@ -17,6 +18,8 @@ class Layout(QGridLayout):
         self.mainWindow = mainWindow
 
         self.sch = pd.read_csv(schedule_path, parse_dates=['Date', 'Return Request Date'], dtype=str)
+
+        self.member = pd.read_csv(member_path, dtype=str)
 
         self.selectedSchedules = []
 
@@ -65,24 +68,38 @@ class Layout(QGridLayout):
         end = pd.to_datetime(self.endDate.date().toPyDate())
         table = self.sch[(self.sch['Date'] >= start) & (self.sch['Date'] <= end)]
         table = table[['Date', 'Child Num', 'Tracking Num', 'Step', 'Movierang', 'Books']]
-
-        # table['Date'] =
+        table['Date'] = [date.date() for date in table['Date']]
 
         self.scheduleTable.setRowCount(table.shape[0])
         self.scheduleTable.setColumnCount(6)
 
-        curRow = 0
         for row in table.index:
-            for col in range(self.scheduleTable.columnCount()):
-                value = table.loc[row][col]
-                item = QTableWidgetItem(str(value))
-                self.scheduleTable.setItem(curRow, col, item)
-
-            curRow += 1
+            self.add_row(table.loc[row], row - table.index[0])
 
         self.scheduleTable.resizeColumnsToContents()
 
-    # def addRow(self):
+    def add_row(self, line, row):
+        self.add_date(line['Date'], row)
+        self.add_name(line['Child Num'], row)
+
+    def add_date(self, date, row):
+        value = "{0.year}-{0.month}-{0.day}".format(date)
+        item = QTableWidgetItem(value)
+        self.scheduleTable.setItem(row, 0, item)
+
+    def add_name(self, num, row):
+        value = self.get_child_name(num)
+        item = QTableWidgetItem(value)
+        self.scheduleTable.setItem(row, 1, item)
+
+    def get_child_name(self, num):
+        family_ID = num[:2]
+        child_ID = num[2:]
+
+        children = self.member[self.member['Family ID'] == family_ID]['Child'].values[0]
+        for child in children.split('/'):
+            if child.split(',')[0] == child_ID:
+                return child.split(',')[1]
 
 
 class Window(QDialog):
