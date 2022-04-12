11f08f4c88479b83cace07c79c6f2f895f63ba93
ihan <angmusae2015@gmail.com>
2022-04-13 01:22:13 +0900

    now filling schedule table works with Schedule.json file
    To do: setting schedule table's row count

--- source/app/DeliveryNotice/ScheduleSelectionWindow.py
+++ source/app/DeliveryNotice/ScheduleSelectionWindow.py
@@ -4,10 +4,11 @@ from PyQt5.QtCore import Qt
 import pandas as pd
 import MemberTools
 import datetime
+import json
 
 
 data_path = "../data/"
-schedule_path = data_path + "Schedule.csv"
+schedule_path = data_path + "Schedule.json"
 book_list_path = data_path + "BookList.csv"
 series_list_path = data_path + "SeriesList.csv"
 member_path = data_path + "Member.csv"
@@ -22,9 +23,11 @@ class Layout(QGridLayout):
 
         self.main_window = main_window
 
-        self.sch = pd.read_csv(schedule_path, parse_dates=['Date'], dtype=str)
-        self.sch = self.sch[self.sch.columns[:5]]
-        self.sch.fillna('', inplace=True)
+        # self.sch = pd.read_csv(schedule_path, parse_dates=['date'], dtype=str)
+        # self.sch.fillna('', inplace=True)
+
+        with open(schedule_path) as f:
+            self.sch = json.load(f)
 
         self.showedSchedules = []
         self.selectedSchedules = []
@@ -41,7 +44,7 @@ class Layout(QGridLayout):
 
         self.scheduleTable = ScheduleTable(self)
 
-        self.searchButton = FindButton(self)
+        self.findButton = FindButton(self)
 
         self.selectedStatus = SelectedStatus(self)
 
@@ -51,7 +54,7 @@ class Layout(QGridLayout):
         self.addWidget(self.endDateLabel, 0, 5, 1, 2)
         self.addWidget(self.startDate, 1, 0, 1, 5)
         self.addWidget(self.endDate, 1, 5, 1, 5)
-        self.addWidget(self.searchButton, 2, 9)
+        self.addWidget(self.findButton, 2, 9)
         self.addWidget(self.scheduleTable, 4, 0, 5, 10)
         self.addWidget(self.selectedStatus, 10, 0)
         self.addWidget(self.addMsgButton, 10, 9)
@@ -59,8 +62,6 @@ class Layout(QGridLayout):
 
         self.startDate.dateChanged.connect(self.change_end_date_range)
         self.endDate.dateChanged.connect(self.change_start_date_range)
-        self.searchButton.clicked.connect(self.fill_table)
-        self.searchButton.clicked.connect(self.reset_selected_row)
         self.addMsgButton.clicked.connect(self.main_window.add_and_exit)
 
     def change_start_date_range(self):
@@ -69,40 +70,6 @@ class Layout(QGridLayout):
     def change_end_date_range(self):
         self.endDate.setMinimumDate(self.startDate.date())
 
-    def fill_table(self):
-        start = pd.to_datetime(self.startDate.date().toPyDate())
-        end = pd.to_datetime(self.endDate.date().toPyDate())
-        scheduleDf = self.sch[(self.sch['Date'] >= start) & (self.sch['Date'] <= end)]
-        scheduleDf['Date'] = [date.date() for date in scheduleDf['Date']]
-
-        self.scheduleTable.setRowCount(scheduleDf.shape[0])
-
-        funcList = [
-            date_table_item,
-            child_name_table_item,
-            tracking_num_table_item,
-            QTableWidgetItem,
-            QTableWidgetItem
-        ]
-
-        for row in scheduleDf.index:
-            rowIndex = row - scheduleDf.index[0]
-            checkBox = TableCheckBox(self, rowIndex)
-            self.scheduleTable.setCellWidget(rowIndex, 0, checkBox)
-
-            for column in scheduleDf.columns:
-                columnIndex = list(scheduleDf.columns).index(column)
-                value = scheduleDf.loc[row, column]
-
-                self.scheduleTable.setItem(row - scheduleDf.index[0], columnIndex + 1, funcList[columnIndex](value))
-
-        self.scheduleTable.resizeColumnsToContents()
-        self.scheduleTable.resizeRowsToContents()
-
-    def reset_selected_row(self):
-        self.scheduleTable.selectedRows = []
-        self.selectedStatus.changeState(0)
-
 
 class ScheduleTable(QTableWidget):
     def __init__(self, main_layout):
@@ -110,7 +77,7 @@ class ScheduleTable(QTableWidget):
 
         self.main_layout = main_layout
 
-        self.headers = ['', '배송일', '학생', '운송장 번호', '책', '무비랑']
+        self.headers = ['', '운송장 번호', '배송일', '학생', '책', '무비랑']
         self.selectedRows = []
 
         self.setColumnCount(len(self.headers))
@@ -124,8 +91,68 @@ class FindButton(QPushButton):
         super().__init__()
 
         self.main_layout = main_layout
-        self.table = self.main_layout.scheduleTable
         self.setText('찾기')
+
+        self.clicked.connect(self.reset_table)
+        self.clicked.connect(self.fill_table)
+        self.clicked.connect(self.reset_selected_row)
+
+    def reset_table(self):
+        self.main_layout.scheduleTable.setRowCount(0)
+
+    def fill_table(self):
+        sch_in_range = [sch for sch in self.main_layout.sch if self.date_filter(sch)]
+
+        # self.main_layout.scheduleTable.setRowCount(len(sch_in_range))
+        self.main_layout.scheduleTable.setRowCount(20)
+
+        crt_row = 0
+        for row in sch_in_range:
+            child_num = len(row['content'])
+
+            if child_num > 1:
+                for col in range(3):
+                    self.main_layout.scheduleTable.setSpan(crt_row, col, child_num, 1)
+
+            checkbox = TableCheckBox(self.main_layout, crt_row)
+            self.main_layout.scheduleTable.setCellWidget(crt_row, 0, checkbox)
+
+            item = tracking_num_table_item(row['tracking num'])
+            self.main_layout.scheduleTable.setItem(crt_row, 1, item)    # add tracking number to table
+
+            item = QTableWidgetItem(row['date'])
+            self.main_layout.scheduleTable.setItem(crt_row, 2, item)    # add date to table
+
+            for n in range(child_num):
+                content = row['content'][n]
+                item = child_name_table_item(content['child'])
+                self.main_layout.scheduleTable.setItem(crt_row, 3, item)
+
+                if content['book'] != 'SAME':
+                    book_num = 0
+                    for book in content['book']:
+                        book_num += len(book['books'])
+
+                    first_book = (content['book'][0]['series'], content['book'][0]['books'][0])
+                    item = QTableWidgetItem(f"{first_book[0]}-{first_book[1]} 외 {book_num - 1}권")
+                    self.main_layout.scheduleTable.setItem(crt_row, 4, item)
+
+                crt_row += 1
+
+        self.main_layout.scheduleTable.resizeColumnsToContents()
+        self.main_layout.scheduleTable.resizeRowsToContents()
+
+    def date_filter(self, sch):
+        start = self.main_layout.startDate.date().toPyDate()
+        end = self.main_layout.endDate.date().toPyDate()
+
+        date = datetime.datetime.strptime(sch['date'], '%Y-%m-%d').date()
+
+        return (date > start) and (date < end)
+
+    def reset_selected_row(self):
+        self.main_layout.scheduleTable.selectedRows = []
+        self.main_layout.selectedStatus.changeState(0)
 
 
 class TableCheckBox(QWidget):
@@ -180,9 +207,11 @@ def to_table_item(func):
     return wrapper
 
 
+"""
 @to_table_item
 def date_table_item(date: datetime.date):
     return "{0.year}-{0.month}-{0.day}".format(date)
+"""
 
 
 @to_table_item
