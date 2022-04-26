455d886d8361468d3b6f18c3a2388e98296467d4
angmusae2015 <angmusae2015@gmail.com>
2022-04-26 15:31:07 +0900

    Use Series.json as book database instead of SeriesList.csv

--- source/app/BookTools.py
+++ source/app/BookTools.py
@@ -1,13 +1,8 @@
-import pandas as pd
 import json
 
 series_path = "../data/Series.json"
 with open(series_path, encoding='utf-8') as f:
     series_dic = json.load(f)
-
-
-book_path = "../data/BookList.csv"
-book_df = pd.read_csv(book_path, dtype=str)
 
 
 def get_series_name(code: str):
@@ -16,8 +11,7 @@ def get_series_name(code: str):
     return name
 
 
-def get_book_name(series_code: str, num: str):
-    series_df = book_df[book_df['Series'] == series_code]
-    book_name = series_df[series_df['Num'] == num]['Name'].values[0]
+def get_book_name(series_code: str, vol: str):
+    book_name = series_dic[series_code][vol]['Name']
 
     return book_name
